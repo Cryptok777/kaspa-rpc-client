839797a7f4bb8f054614ba19b44f9b7ee0641adc
kkhhaalil <119089486+Cryptok777@users.noreply.github.com>
2023-04-05 22:52:22 -0400

    Update demo.ts

--- demo.ts
+++ demo.ts
@@ -1,95 +1,76 @@
-import { KaspadClientWrapper } from './lib/KaspadClientWrapper'
+const { KaspadClientWrapper, KaspadWallet } = require('kaspa-rpc-client')
 
 const run = async () => {
+
+
+  const a = {
+  }
+  console.log(a);
+
+
+  const address = await KaspadWallet.mnemonicToSeed(a.phrase, "password")
+  // console.log(c);
+
+
+
   // Initilize the clients
   const wrapper = new KaspadClientWrapper({
-    hosts: ["kaspadns.kaspacalc.net:16110"],
-    // verbose: true, ---> Verbose log
+
+    // verbose: true,
   })
+
   await wrapper.initialize()
 
   // Get one client from the pool
   const client = await wrapper.getClient()
 
-  // Example method calls for KaspadClient class methods starting with "get":
+  const balance = await client.getBalanceByAddress({ address })
+  console.log(balance);
 
-  // Get a block by hash:
-  const block = await client.getBlock({
-    hash: "6ef1913d30316304254aa5ce6c34ff9dd2b519231bb80194d4b5e5449412e924",
-    includeTransactions: true,
-  })
-  console.log(block)
+  // Step 1: get all available UTXOs
+  const utxos = await client.getUtxosByAddresses({ addresses: [address] })
+  console.log(JSON.stringify(utxos, null, 4));
 
-  // Get unspent transaction outputs (UTXOs) for a list of addresses:
-  const utxos = await client.getUtxosByAddresses({
-    addresses: [
-      "kaspa:qrrzeucwfetuty3qserqydw4z4ax9unxd23zwp7tndvg7cs3ls8dvwldeayv5",
+  const tempUtxos = {
+    "entries": [
+      {
+        "address": "kaspa:qpc0f3mhherpp026zfng3sq5j3gkqswacjv8wlvpf2v48k863fpluhpt0tkqj",
+        "outpoint": {
+          "transactionId": "59eba628955546b1a52828320c6b9fd0adcda3ef7c585ee0d5e81b2602c9f746",
+          "index": 0
+        },
+        "utxoEntry": {
+          "amount": BigInt("10000"),
+          "scriptPublicKey": {
+            "version": 0,
+            "scriptPublicKey": "2070f4c777be4610bd5a126688c01494516041ddc498777d814a9953d8fa8a43feac"
+          },
+          "blockDaaScore": "44302564",
+          "isCoinbase": false
+        }
+      }
     ],
-  })
-  console.log(utxos)
+    "error": null
+  }
 
-  // Get the balance for a specific address:
-  const balance = await client.getBalanceByAddress({
-    address:
-      "kaspa:qrrzeucwfetuty3qserqydw4z4ax9unxd23zwp7tndvg7cs3ls8dvwldeayv5",
-  })
-  console.log(balance)
+  // Step 2: Create utxo set
+  let utxoSet = KaspadWallet.getUtxoSet(tempUtxos)
+  console.log(utxoSet);
 
-  // Get balances for a list of addresses:
-  const balances = await client.getBalancesByAddresses({
-    addresses: [
-      "kaspa:qrrzeucwfetuty3qserqydw4z4ax9unxd23zwp7tndvg7cs3ls8dvwldeayv5",
-    ],
-  })
-  console.log(balances)
 
-  // Get the virtual selected parent blue score:
-  const vsbs = await client.getVirtualSelectedParentBlueScore()
-  console.log(vsbs)
 
-  // Get the latest block template:
-  const blockTemplate = await client.getBlockTemplate({
-    payAddress:
-      "kaspa:qrrzeucwfetuty3qserqydw4z4ax9unxd23zwp7tndvg7cs3ls8dvwldeayv5",
-    extraData: "",
-  })
-  console.log(blockTemplate)
+  // client.subscribeUtxosChanged({ addresses: ['kaspa:qpc0f3mhherpp026zfng3sq5j3gkqswacjv8wlvpf2v48k863fpluhpt0tkqj'] }, (res) => {
+  //   console.log(JSON.stringify(res, null, 4));
+  // })
 
-  // Get the current state of the block DAG:
-  const blockDagInfo = await client.getBlockDagInfo()
-  console.log(blockDagInfo)
+  // const val = await client.getBlock({hash: "8586ca39ac7a742aa93cdbb6489ec596fdfb568e34da3180bbcbf3b8f215c557", includeTransactions: true})
+  // console.log(val);
 
-  // Get information about the node's current state:
-  const info = await client.getInfo()
-  console.log(info)
-
-  // Get an estimate of the current network's hashes per second:
-  const networkHashRate = await client.estimateNetworkHashesPerSecond({
-    windowSize: 1000,
-    startHash:
-      "6ef1913d30316304254aa5ce6c34ff9dd2b519231bb80194d4b5e5449412e924",
-  })
-  console.log(networkHashRate)
-
-  // Get the current coin supply:
-  const coinSupply = await client.getCoinSupply()
-  console.log(coinSupply)
-
-  // Example notifier calls
-  client.subscribeUtxosChanged(
-    {
-      addresses: [
-        "kaspa:qrrzeucwfetuty3qserqydw4z4ax9unxd23zwp7tndvg7cs3ls8dvwldeayv5",
-      ],
-    },
-    (res) => {
-      console.log(
-        `subscribeUtxosChanged callback: ${JSON.stringify(res, null, 4)}`
-      )
-    }
-  )
+  // client.subscribeVirtualSelectedParentBlueScoreChanged((aa) => {
+  //   console.log(aa.virtualSelectedParentBlueScore);
+  // })
 }
 
-;(async () => {
-  await run()
-})()
+  ; (async () => {
+    await run()
+  })()
