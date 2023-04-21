b0b5f32cda27bd967114e0e61e663be1ad09da2e
kkhhaalil <119089486+Cryptok777@users.noreply.github.com>
2023-04-20 23:47:49 -0400

    Update ClientProvider.ts

--- lib/ClientProvider.ts
+++ lib/ClientProvider.ts
@@ -7,37 +7,37 @@ export class ClientProvider {
   constructor() {}
 
   getUtxosByAddresses(
-    data: RPC.GetUtxosByAddressesRequestMessage
+    _data: RPC.GetUtxosByAddressesRequestMessage
   ): Promise<RPC.GetUtxosByAddressesResponseMessage> {
     throw new Error("Not implemented")
   }
 
   getBalanceByAddress(
-    data: RPC.GetBalanceByAddressRequestMessage
+    _data: RPC.GetBalanceByAddressRequestMessage
   ): Promise<RPC.GetBalanceByAddressResponseMessage> {
     throw new Error("Not implemented")
   }
 
   getBalancesByAddresses(
-    data: RPC.GetBalancesByAddressesRequestMessage
+    _data: RPC.GetBalancesByAddressesRequestMessage
   ): Promise<RPC.GetBalancesByAddressesResponseMessage> {
     throw new Error("Not implemented")
   }
 
   submitTransaction(
-    data: RPC.SubmitTransactionRequestMessage
+    _data: RPC.SubmitTransactionRequestMessage
   ): Promise<RPC.SubmitTransactionResponseMessage> {
     throw new Error("Not implemented")
   }
 
   subscribeUtxosChanged(
-    data: RPC.NotifyUtxosChangedRequestMessage,
-    callback: RPC.callback<RPC.UtxosChangedNotificationMessage>
+    _data: RPC.NotifyUtxosChangedRequestMessage,
+    _callback: RPC.callback<RPC.UtxosChangedNotificationMessage>
   ): RPC.SubPromise<RPC.NotifyUtxosChangedResponseMessage> {
     throw new Error("Not implemented")
   }
 
-  unSubscribeUtxosChanged(uid = "") {
+  unSubscribeUtxosChanged(_uid = "") {
     throw new Error("Not implemented")
   }
 }
